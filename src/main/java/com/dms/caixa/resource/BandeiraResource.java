e69388a64843f9a9872e8cb54d549a241aab49a9
Diorgenes Morais <diorgenesmorais@gmail.com>
2019-01-06 15:39:09 -0300

    criei BandeiraResource

---
+++ src/main/java/com/dms/caixa/resource/BandeiraResource.java
@@ -0,0 +1,27 @@
+package com.dms.caixa.resource;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dms.caixa.model.Bandeira;
+import com.dms.caixa.repository.BandeirasRepository;
+
+@RestController
+@RequestMapping("/bandeiras")
+public class BandeiraResource {
+
+	@Autowired
+	private BandeirasRepository bandeirasRepository;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscarPorId(@PathVariable Integer id) {
+		Optional<Bandeira> bandeira = bandeirasRepository.findById(id);
+		return ResponseEntity.of(bandeira);
+	}
+}
