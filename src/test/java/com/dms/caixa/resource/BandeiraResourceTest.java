8a60178aba336c54e9ffa818d3ae5663e7b329dc
Diorgenes Morais <diorgenesmorais@gmail.com>
2019-06-24 18:35:24 -0300

    definir mais um teste p/ BandeiraResource

--- src/test/java/com/dms/caixa/resource/BandeiraResourceTest.java
+++ src/test/java/com/dms/caixa/resource/BandeiraResourceTest.java
@@ -1,23 +1,52 @@
 package com.dms.caixa.resource;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
 
 import com.dms.caixa.BaseTest;
 
+import io.restassured.http.ContentType;
+
 public class BandeiraResourceTest extends BaseTest {
 
+	private String recurso = "bandeiras";
+	private Integer id = 6;
+
 	@Test
-	public void deveObterUmaBandeira() throws Exception {
+	public void deveObterUmaBandeiraEmJson() throws Exception {
 		given()
+			.accept(ContentType.JSON) // não é necessário informar
+			.pathParam("recurso", recurso)
+			.pathParam("id", id)
 		.when()
-			.get("/bandeiras/1")
+			.get("/{recurso}/{id}")
 		.then()
+			.log().all()
 			.assertThat()
 			.statusCode(200)
-			.body("id", is(1))
+			.body("id", is(6))
+			.body("nome", is("Maestro"))
+			.body("descricao", is("Redeshop débito"))
+			;
+	}
+
+	@Test
+	public void deveObterBandeiraEmXML() throws Exception {
+		given()
+			.accept(ContentType.XML)
+			.pathParam("recurso", recurso)
+			.pathParam("id", id)
+		.when()
+			.get("/{recurso}/{id}")
+		.then()
+			.log().all()
+			.assertThat()
+			.statusCode(200)
+			.rootPath("Bandeira")
+			.body("id", is("6"))
+			.body("nome", is("Maestro"))
 			;
 	}
 }
